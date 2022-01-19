f8967e90a75e29e8a8718c516dff3d3af8a40b87
Lê Minh Toàn <mmminhle771@gmail.com>
2022-01-19 21:42:06 +0700

    responsive

--- js/script.js
+++ js/script.js
@@ -6,7 +6,35 @@ $(document).ready(function () {
         draggable: false,
         prevArrow: "<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow: "<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
+});
+
+//Header toggle
+const menu = document.querySelector(".header__menu");
+const btnBar = document.querySelector(".header__btn-bars");
+const btnClose = document.querySelector(".header__btn-close");
+
+btnBar.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+        menu.classList.add("active");
+    }
+
+    document.body.style.overflow = "hidden";
+});
+
+btnClose.addEventListener("click", () => {
+    menu.classList.remove("active");
+
+    document.body.style.overflow = "auto";
 });
 
 // BENEFIT TABS
